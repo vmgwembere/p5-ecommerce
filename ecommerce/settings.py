c77a7dbf424873ae65ac367fc10c872420da2a1b
Veronica Gwembere <vmgwembere@gmail.com>
2019-01-15 19:49:18 +0000

    Removed env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env 
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -85,6 +84,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
+#if "DATABASE_URL" in os.environ:
+#    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+#else:
+#    print("Database URL not found. Using SQLite instead")
+
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 else:
@@ -94,8 +98,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
-
+   }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
